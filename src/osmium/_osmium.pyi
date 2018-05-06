a19db0dccb56057bb046e8c7dd8a64739a272bef
Wiktor Niesiobedzki <github@vink.pl>
2018-05-06 12:26:07 +0200

    Create BaseHandler as superclass of WriteHandler and SimpleHandler.
    Fix NodeLocationsForWays.ignore_errors - it's a function

--- src/osmium/_osmium.pyi
+++ src/osmium/_osmium.pyi
@@ -15,7 +15,7 @@ class MergeInputReader(Reader):
 
     def add_file(self, file: str) -> int: ...
 
-    def apply(self, handler: WriteHandler, idx: typing.Optional[str], simplify: bool = ...): ...
+    def apply(self, handler: BaseHandler, idx: typing.Optional[str], simplify: bool = ...): ...
 
     def apply_to_reader(self, reader: Reader, writer: Writer, with_history: bool = ...): ...
 
@@ -24,14 +24,10 @@ LocationTable = typing.Mapping[int, Location]
 
 
 class NodeLocationsForWays(LocationTable):
-    ignore_errors: bool  # ???
+    def ignore_errors(self): ...
 
 
-class SimpleHandler:
-    def apply_buffer(self, buffer: bytes, format: str, locations: bool = ..., idx: str = ...): ...
-
-    def apply_file(self, filename: str, locations: bool = ..., idx: str = ...): ...
-
+class BaseHandler:
     def node(self, node: Node): ...
 
     def way(self, way: Way): ...
@@ -41,6 +37,14 @@ class SimpleHandler:
     def area(self, area: Area): ...
 
     def changeset(self, changeset: Changeset): ...
+
+    def apply_start(self): ...
+
+
+class SimpleHandler(BaseHandler):
+    def apply_buffer(self, buffer: bytes, format: str, locations: bool = ..., idx: str = ...): ...
+
+    def apply_file(self, filename: str, locations: bool = ..., idx: str = ...): ...
 
 
 class SimpleWriter:
@@ -55,12 +59,12 @@ class SimpleWriter:
     def close(self): ...
 
 
-class WriteHandler:
+class WriteHandler(BaseHandler):
     def __init__(self, arg2: str, arg3: typing.Optional[int]): ...  # filename, buffer_size
 
 
 @typing.overload
-def apply(arg1: Reader, arg2: WriteHandler): ...  # reader, write_handler
+def apply(arg1: Reader, arg2: BaseHandler): ...  # reader, write_handler
 
 
 @typing.overload
@@ -68,4 +72,4 @@ def apply(arg1: Reader, arg2: NodeLocationsForWays): ...  # reader, nodes_idx
 
 
 @typing.overload
-def apply(arg1: Reader, arg2: NodeLocationsForWays, arg3: WriteHandler): ...  # reader, nodes_idx, write_handler
+def apply(arg1: Reader, arg2: NodeLocationsForWays, arg3: BaseHandler): ...  # reader, nodes_idx, write_handler
